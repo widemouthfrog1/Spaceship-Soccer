624f09a7285ce821b32abf30e84e90875cf445a0
Karl Bennett <widemouthfrog1@gmail.com>
2021-01-21 16:41:50 +1300

    Add empty scroll menu

---
+++ Assets/ButtonList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonList : MonoBehaviour
+{
+    public GameObject baseItem;
+    public List<GameObject> items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
